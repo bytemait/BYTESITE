9d8d95551966004a5cd7081769b341fd30afab1b
Nalin <nalinkhanna17@gmail.com>
2024-08-09 21:57:59 +0530

    achievement page and timeline component added

---
+++ src/pages/timelineelements.js
@@ -0,0 +1,63 @@
+let timelineElements = [
+    {
+      id: 1,
+      title: "SIH 2023 Winners ",
+      location: "Delhi , India",
+      description:
+        "Congratulations to Ishaan and Aman for winning the prestigious Smart India Hackathon 2023! Their innovative solution stood out among 15,000 teams, earning them the top spot.",
+      date: "August 2023",
+      icon: "byte",
+    },
+    {
+      id: 2,
+      title: "SIH 2023 Winners ",
+      location: "Delhi , India",
+      description:
+        "Congratulations to Ishaan and Aman for winning the prestigious Smart India Hackathon 2023! Their innovative solution stood out among 15,000 teams, earning them the top spot.",
+      buttonText: "View Backend Projects",
+      date: "August 2023 ",
+      icon: "byte",
+    },
+    {
+      id: 3,
+      title: "SIH 2023 ",
+      location: "Delhi , India",
+      description:
+        "Congratulations to Ishaan and Aman for winning the prestigious Smart India Hackathon 2023! Their innovative solution stood out among 15,000 teams, earning them the top spot.",
+      buttonText: "Company Website",
+      date: "August 2023",
+      icon: "byte",
+    },
+    {
+        id: 4,
+        title: "SIH 2023 ",
+        location: "Delhi , India",
+        description:
+          "Congratulations to Ishaan and Aman for winning the prestigious Smart India Hackathon 2023! Their innovative solution stood out among 15,000 teams, earning them the top spot.",
+        buttonText: "Company Website",
+        date: "August 2023",
+        icon: "byte",
+      },
+      {
+        id: 5,
+        title: "SIH 2023 ",
+        location: "Delhi , India",
+        description:
+          "Congratulations to Ishaan and Aman for winning the prestigious Smart India Hackathon 2023! Their innovative solution stood out among 15,000 teams, earning them the top spot.",
+        buttonText: "Company Website",
+        date: "August 2023",
+        icon: "byte",
+      },
+      {
+        id: 6,
+        title: "SIH 2023 ",
+        location: "Delhi , India",
+        description:
+          "Congratulations to Ishaan and Aman for winning the prestigious Smart India Hackathon 2023! Their innovative solution stood out among 15,000 teams, earning them the top spot.",
+        buttonText: "Company Website",
+        date: "August 2023",
+        icon: "byte",
+      },
+  ];
+  
+  export default timelineElements;
