095329d233b28ef0420a770c26ad79ec3d204956
AdishtJaglan <adisht7524@gmail.com>
2024-08-09 18:53:29 +0530

    port to vite, setup tw themes

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,7 @@
+export default function Projects() {
+  return (
+    <>
+      <h1 className="bg-theme-1 text-theme-2">Projects</h1>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "theme-1": "#4afaab", // green
+        "theme-2": "#000319", // black blueish
+      },
+    },
+  },
+  plugins: [],
+};
