a5de4eec8efefdfbb643333bf4e7437aa55e92a5
Inferno2211 <abirjerry2211@gmail.com>
2024-08-22 01:22:06 +0530

    home page

--- src/components/Home-Hero-Section/HeroMid.jsx
+++ src/components/Home-Hero-Section/HeroMid.jsx
@@ -11,7 +11,7 @@ const HeroMid = () => {
       { x: containerWidth },
       {
         x: -textWidth,
-        duration: 10, // Adjust the duration for speed
+        duration: 20, // Adjust the duration for speed
         ease: "none",
         repeat: -1, // Infinite loop
         repeatDelay: 0,
@@ -22,7 +22,8 @@ const HeroMid = () => {
   return (
     <div className="hero-mid-container overflow-hidden text-6xl font-bold mb-4 text-white">
       <div className="hero-mid-text inline-block whitespace-nowrap">
-        Unravelling complexity, forging success. Join us at <span className="text-[#4afaab]">BYTE</span> where innovation meets action
+      Mutual Fund investments are subject to market risks, read all scheme related documents carefully. The NAVs of the schemes may go up or down depending upon the factors and forces affecting the securities market including the fluctuations in the interest rates. The past performance of the mutual funds is not necessarily indicative of future performance of the schemes. The Mutual Fund is not guaranteeing or assuring any dividend under any of the schemes and the same is subject to the availability and adequacy of distributable surplus. Investors are requested to review the prospectus carefully and obtain expert professional advice with regard to specific legal, tax and financial implications of the investment/participation in the scheme.
+      {/* <span className="text-[#4afaab]">BYTE</span> where innovation meets action */}
       </div>
     </div>
   );
