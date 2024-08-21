aac3291efcf1be87a0ba9934e0cffbfc8edafa18
Nalin <nalinkhanna17@gmail.com>
2024-08-21 11:55:16 +0530

    about us page me model dal diya

---
+++ src/components/about_section/index.js
@@ -0,0 +1,8 @@
+import Hero from './Hero';
+import About from './Aboutm';
+import Tech from './Tech';
+export {
+    Hero,
+    About,
+    Tech
+}
---
+++ src/pages/Aboutus.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Hero from '../components/about_section/Hero'
+import Aboutm from '../components/about_section/Aboutm'
+import Tech from '../components/about_section/Tech'
+const Aboutus = () => {
+  return (
+    <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Hero/>
+          
+        </div>
+        <Aboutm/>
+        <Tech/>
+      </div>
+  
+  )
+}
+
+export default Aboutus
