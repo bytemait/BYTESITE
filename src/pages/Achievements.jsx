6d4e25538fd52e0ce6a405ca330c6816abe9dd78
Nalin <nalinkhanna17@gmail.com>
2024-08-22 10:50:49 +0530

    achievements animation improved

--- src/pages/Achievements.jsx
+++ src/pages/Achievements.jsx
@@ -5,10 +5,25 @@ import { experiences } from '../components/constants';
 import {SectionWrapper} from '../components/hoc'
 import { textVariant } from '../components/utils/motion';
 import {styles} from '../components/style'
+import { useRef } from 'react';
+import {Tilt} from 'react-tilt'
 
-const ExperienceCard = ({ experience }) => {
+const ExperienceCard = ({ experience ,scrollToElement }) => {
+  const elementRef = useRef(null);
+
+  const handleClick = () => {
+    elementRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'start',
+      duration: 3000,
+      timingFunction: 'ease-in-out'
+
+    });
+  };
   return (
     <VerticalTimelineElement
+      ref={elementRef}
       contentStyle={{
         background: "#1d1836",
         color: "#fff",
@@ -16,6 +31,7 @@ const ExperienceCard = ({ experience }) => {
       contentArrowStyle={{ borderRight: "7px solid  #4afaab" }}
       date={experience.date}
       iconStyle={{ background: '#4afaab' }}
+      onClick = {handleClick}
     >
       <div>
         <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
@@ -37,13 +53,14 @@ const ExperienceCard = ({ experience }) => {
           </li>
         ))}
       </ul>
+        
     </VerticalTimelineElement>
   );
 };
 
 function Achievements() {
   return (
-    <>
+    <div className='bg-[#000319]'>
       <motion.div variants={textVariant()}>
       <p className={`${styles.sectionSubText} text-center`}>
           What we have achieved so far
@@ -53,7 +70,10 @@ function Achievements() {
         </h2>
       </motion.div>
       <div className='mt-20 flex flex-col '>
-        <VerticalTimeline>
+        <VerticalTimeline lineColor='#4afaab'
+         scrollBehavior = 'smooth'
+         scrollSpeed = '50'
+        >
           {experiences.map((experience, index) => (
             <ExperienceCard
               key={`experience-${index}`}
@@ -62,7 +82,7 @@ function Achievements() {
           ))}
         </VerticalTimeline>
       </div>
-    </>
+    </div>
   );
 }
 
