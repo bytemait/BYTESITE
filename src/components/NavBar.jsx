69d20cec2c629a5a96c0d95d8faaf65497a4521f
Harshill Aggarwal <harshillsms@gmail.com>
2024-08-23 15:19:00 +0530

    fixed navbar import and sticky and projects page made responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,12 +3,12 @@ import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div className="w-screen h-20  flex justify-center gap-8 items-center p-2 fixed top-0 z-50">
+    <div className="h-20 flex justify-center md:gap-8 gap-5 items-center p-2 bg-black">
       <NavLink
         to="/home"
         className={({ isActive }) =>
           `text-white  ${isActive ? "border-b-2 border-white" : ""}  border-b-2 border-transparent
-           hover:border-b-2 hover:border-white transition-all duration-300 font-ocr text-lg tracking-widest font-medium`
+           hover:border-b-2 hover:border-white transition-all duration-300 font-ocr md:text-lg text-sm tracking-widest font-medium`
         }
       >
         Home
@@ -17,7 +17,7 @@ function NavBar() {
         to="/achievements"
         className={({ isActive }) =>
           `text-white  ${isActive ? "border-b-2 border-white" : ""}  border-b-2 border-transparent
-           hover:border-b-2 hover:border-white transition-all duration-300 font-ocr text-lg tracking-widest font-medium`
+           hover:border-b-2 hover:border-white transition-all duration-300 font-ocr md:text-lg text-sm tracking-widest font-medium`
         }
       >
         Achievements
@@ -26,7 +26,7 @@ function NavBar() {
         to="/projects"
         className={({ isActive }) =>
           `text-white  ${isActive ? "border-b-2 border-white" : ""}  border-b-2 border-transparent
-           hover:border-b-2 hover:border-white transition-all duration-300 font-ocr text-lg tracking-widest font-medium`
+           hover:border-b-2 hover:border-white transition-all duration-300 font-ocr md:text-lg text-sm tracking-widest font-medium`
         }
       >
         Projects
@@ -35,7 +35,7 @@ function NavBar() {
         to="/about"
         className={({ isActive }) =>
           `text-white  ${isActive ? "border-b-2 border-white" : ""}  border-b-2 border-transparent
-           hover:border-b-2 hover:border-white transition-all duration-300 font-ocr text-lg tracking-widest font-medium`
+           hover:border-b-2 hover:border-white transition-all duration-300 font-ocr md:text-lg text-sm tracking-widest font-medium`
         }
       >
         About Us
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,26 +3,25 @@ import Carousel from "../components/Carousel/Carousel";
 export default function Projects() {
   return (
     <div className="h-screen bg-gradient-to-b from-[#000000] to-[#032703]">
-      <div className="flex flex-col justify-center items-center pt-32">
-        <div className="flex items-end gap-6 mr-48">
-          <span className="text-9xl font-extrabold tracking-widest bg-gradient-to-b from-white to-[#999999] bg-clip-text text-transparent">
+      <div className="flex flex-col justify-center items-center">
+        <div className="flex items-end gap-6 max-w-fit mt-4">
+          <span className="lg:text-9xl md:text-8xl text-6xl  font-extrabold tracking-widest bg-gradient-to-b from-white to-[#999999] bg-clip-text text-transparent">
             BYTE
           </span>
           <div className="flex flex-col tracking-widest">
-            <span className="text-[#4AFAAB] text-2xl font-bold">
+            <span className="text-[#4AFAAB] lg:text-2xl text-sm font-bold">
               Solving Problems
             </span>
-            <span className="text-[#4AFAAB] text-2xl font-bold">
+            <span className="text-[#4AFAAB] lg:text-2xl text-sm font-bold">
               One Byte at a Time
             </span>
           </div>
         </div>
-        <p className="text-9xl font-extrabold tracking-widest bg-gradient-to-b from-white to-[#999999] bg-clip-text text-transparent mt-6 ml-10">
+        <p className="lg:text-9xl md:text-8xl text-6xl font-extrabold tracking-widest bg-gradient-to-b from-white to-[#999999] bg-clip-text text-transparent mt-6 ml-10">
           PROJECTS
         </p>
       </div>
-      <div className="App mt-20">
-        {" "}
+      <div className="App lg:mt-3 md:mt-8 mt-10">
         <Carousel />
       </div>
     </div>
