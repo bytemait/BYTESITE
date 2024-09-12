802bfae3b1cc5e1734a76471e4051ed69f399c18
Harshill Aggarwal <harshillsms@gmail.com>
2024-09-13 02:21:11 +0530

    update carousel design in projects page

--- src/components/Carousel/RippleButton.jsx
+++ src/components/Carousel/RippleButton.jsx
@@ -13,16 +13,19 @@ const RippleButton = ({
   };
 
   return (
-    <div className="circle-container" style={containerStyle}>
-      <div className="large"></div>
-      <div className="medium"></div>
-      <div className="image-circle">
-        <img src={image} alt="Inner Circle" />
+    <div className="">
+      <div className="circle-container" style={containerStyle}>
+        <div className="large"></div>
+        <div className="medium"></div>
+        <div className="image-circle">
+          <img src={image} alt="Inner Circle" />
+        </div>
+        <div className="text above w-[150px]">{textAbove}</div>{" "}
+        {/* Dynamic text above the image */}
+        <a target= "_blank"href="https://github.com/bytemait"><div className="text below">{textBelow}</div>{" "}</a>
+        {/* Dynamic text below the image */}
       </div>
-      <div className="text above w-[150px]">{textAbove}</div>{" "}
-      {/* Dynamic text above the image */}
-      <a target= "_blank"href="https://github.com/bytemait"><div className="text below">{textBelow}</div>{" "}</a>
-      {/* Dynamic text below the image */}
+      
     </div>
   );
 };
---
+++ src/components/Carousel/data.js
@@ -0,0 +1,105 @@
+import image1 from "./image1.png";
+import image2 from "./image2.png";
+
+const buttonsData = [
+  {
+    id: 1,
+    image: image1,
+    size: 280,
+    textAbove: "Object Avoidance Bot",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 2,
+    image: image2,
+    size: 280,
+    textAbove: "ByteLocker",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 3,
+    image: "/projects/bylexa.png",
+    size: 280,
+    textAbove: "Bylexa",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 4,
+    image: "/projects/eduverse.png",
+    size: 280,
+    textAbove: "Eduverse",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 5,
+    image: "/projects/metablocks.png",
+    size: 280,
+    textAbove: "Metablocks",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 6,
+    image: "/projects/progmatic.png",
+    size: 280,
+    textAbove: "Progmatic",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 7,
+    image: "/projects/erp.jpg",
+    size: 280,
+    textAbove: "ERP",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 8,
+    image: image1,
+    size: 280,
+    textAbove: "Object Avoidance Bot",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 9,
+    image: image2,
+    size: 280,
+    textAbove: "ByteLocker",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 10,
+    image: "/projects/bylexa.png",
+    size: 280,
+    textAbove: "Bylexa",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 11,
+    image: "/projects/eduverse.png",
+    size: 280,
+    textAbove: "Eduverse",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 12,
+    image: "/projects/metablocks.png",
+    size: 280,
+    textAbove: "Metablocks",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 13,
+    image: "/projects/progmatic.png",
+    size: 280,
+    textAbove: "Progmatic",
+    textBelow: "Go To Project",
+  },
+  {
+    id: 14,
+    image: "/projects/erp.jpg",
+    size: 280,
+    textAbove: "ERP",
+    textBelow: "Go To Project",
+  },
+];
+
+export default buttonsData;
